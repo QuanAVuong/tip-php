7657b999014f8521411d50ebae55abc505f1bff3
Quan <QuanAVuong@users.noreply.github.com>
2017-01-03 12:37:10 -0500

    initial mdl

--- index.php
+++ index.php
@@ -1,79 +1,95 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Tip Calculator</title>
 
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<!--         <link rel="stylesheet"
+              href="http://fonts.googleapis.com/css?family=Roboto:400,100,500,300italic,500italic,700italic,900,300"> -->
+
         <!-- <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css"> -->
         <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-orange.min.css" />
+
+
         <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+
+
     </head>
 
     <body>
 
-        <?php 
-            include "output.php";
-         ?>
+        <div class="mdl-layout mdl-js-layout">
 
 
-        <h1>Tip Calculator</h1>
+            <?php 
+                include "output.php";
+             ?>
 
-        <form method="POST">
 
-            <label>Bill subtotal: $
-                <input type="text" name="bill" placeholder="Your Bill Amount" value="<?php echo "$bill"; ?>">
-            </label>
-            <span class="error">* <?php echo "$billErr"; ?></span>
-            <br><br>
+            <h1 .mdl-typography--display-2 >Tip Calculator</h1>
 
-            Tip Percentage:
-            <span class="error">* <?php echo "$tip_percentErr"; ?></span>
-            <br><br>
+            <form method="POST">
+
+                <label>Bill subtotal: $
+                    <input type="text" name="bill" placeholder="Your Bill Amount" value="<?php echo "$bill"; ?>">
+                </label>
+                <span class="error">* <?php echo "$billErr"; ?></span>
+                <br><br>
+
+                Tip Percentage:
+                <span class="error">* <?php echo "$tip_percentErr"; ?></span>
+                <br><br>
+
+                <?php
+                    include "percentRadios.php"
+                ?> <br><br>
+
+
+    <!--             <label>
+                    <input type="radio" name="tip_percent"> Custom:
+                </label> 
+                    <input type="text" name="tip_percent_custom" placeholder="eg. 18.5" value="<?php echo "$tip_percent_custom"; ?>" >%
+                    <span class="error">* <?php echo "$tip_percentErr"; ?></span>
+                <br><br> -->
+
+                <label>Split: <input type="text" name="split" value="<?php echo "$split"; ?>"> person(s) </label>
+                <span class="error"><?php echo "$splitErr"; ?></span>
+                <br><br>
+
+
+                <!-- <input type="submit" name="submit"> <br> -->
+                
+                <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" name="submit">
+                  Button
+                </button>
+            </form>
+
 
             <?php
-                include "percentRadios.php"
-            ?> <br><br>
+                if ( isset($_POST["submit"]) ) {
 
+                    // if ( !isset($billErr) && !isset($tip_percentErr) && !isset($splitErr) ) {
+                    if ( empty($arrErr) ) {
+                        echo "
+                            <br> Total Tip: $total_tip
+                            <br> Total (With Tip): $total
+                            <br> Tip Per Person: $tip_each
+                            <br> Total Per Person: $total_each
+                            ";
 
-<!--             <label>
-                <input type="radio" name="tip_percent"> Custom:
-            </label> 
-                <input type="text" name="tip_percent_custom" placeholder="eg. 18.5" value="<?php echo "$tip_percent_custom"; ?>" >%
-                <span class="error">* <?php echo "$tip_percentErr"; ?></span>
-            <br><br> -->
-
-            <label>Split: <input type="text" name="split" value="<?php echo "$split"; ?>"> person(s) </label>
-            <span class="error"><?php echo "$splitErr"; ?></span>
-            <br><br>
-
-
-            <!-- <input type="submit" name="submit"> <br> -->
-            
-            <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" name="submit">
-              Button
-            </button>
-        </form>
-
-
-        <?php
-            if ( isset($_POST["submit"]) ) {
-
-                // if ( !isset($billErr) && !isset($tip_percentErr) && !isset($splitErr) ) {
-                if ( empty($arrErr) ) {
-                    echo "
-                        <br> Total Tip: $total_tip
-                        <br> Total (With Tip): $total
-                        <br> Tip Per Person: $tip_each
-                        <br> Total Per Person: $total_each
-                        ";
-
+                    }
+                    
                 }
-                
-            }
 
-        ?>
+            ?>
+
+
+
+
+        </div>
 
 
     </body>
